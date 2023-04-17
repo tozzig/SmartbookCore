2a3a6b7b2523adceae5d8fd197cb818805b7c08b
Antpn Tsikhanau <anton.tsikhanau@noveogroup.com>
2023-04-17 20:59:55 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# SmartbookCore
+
+A description of this package.
---
+++ Tests/SmartbookCoreTests/SmartbookCoreTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import SmartbookCore
+
+final class SmartbookCoreTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(SmartbookCore().text, "Hello, World!")
+    }
+}
